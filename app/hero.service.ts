8cdd1948f6ff78035c7f8a5a1c99c76f5632a5ee
KJ <kj@neoswell.com>
2016-10-20 18:15:18 +0900

    Make hero-service asynchronous

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,7 +5,14 @@ import { HEROES } from './mock-heros';
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+  // wait 2 seconds
+  getHeroSlowly(): Promise<Hero[]> {
+    return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000)
+           ).then(() => this.getHeroes());
   }
 }
