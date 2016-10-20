d7c0035b568978b4dba5f5300946e0d732612417
KJ <kj@neoswell.com>
2016-10-20 17:39:06 +0900

    Inject hero service into the AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -62,17 +64,25 @@ import { Hero } from './hero';
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [ HeroService ]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  //hero: Hero = {
-  //  id:1,
-  //  name: 'Windstorm'
-  //};
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
