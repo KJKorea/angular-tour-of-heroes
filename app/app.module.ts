ffc9a29deda08b1183a4bbe3c2aa220b17f7c1e0
KJ <kj@neoswell.com>
2016-10-24 10:50:50 +0900

    Providing data with HTTP Services : in-memory web API

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { HeroesComponent }      from './heroes.component';
@@ -8,12 +15,12 @@ import { HeroDetailComponent }  from './hero-detail.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroService }          from './hero.service';
 
-import { AppRoutingModule }     from './app-routing.module';
-
 @NgModule({
   imports:[
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
@@ -22,9 +29,7 @@ import { AppRoutingModule }     from './app-routing.module';
     HeroesComponent,
     HeroDetailComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
